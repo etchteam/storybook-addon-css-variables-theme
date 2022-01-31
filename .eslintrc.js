8adf28facc0e1ff4e44a69c6e7ad8d46e0471a79
Dan Webb <dan@etch.co>
2022-01-31 18:31:31 +0000

    feat: add storybook and simplify the build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,20 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'airbnb-typescript',
-  ],
-  parserOptions: {
-    project: './tsconfig.json',
-  },
-  rules: {
-    "import/no-extraneous-dependencies": ["error", {
-      "devDependencies": true
-    }]
-  }
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ["airbnb-typescript", "plugin:storybook/recommended"],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      rules: {
+        "import/no-extraneous-dependencies": ["error", {
+          "devDependencies": true
+        }],
+        "react/jsx-props-no-spreading": "off"
+      }
+    }
+  ]
 };
---
+++ .storybook/preview.js
@@ -0,0 +1,24 @@
+import cssVariablesTheme from '../src';
+
+import light from '!!style-loader?injectType=lazyStyleTag!css-loader!../stories/assets/light.css';
+import dark from '!!style-loader?injectType=lazyStyleTag!css-loader!../stories/assets/dark.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  cssVariables: {
+    files: {
+      light,
+      dark,
+    }
+  }
+};
+
+export const decorators = [
+  cssVariablesTheme,
+];
