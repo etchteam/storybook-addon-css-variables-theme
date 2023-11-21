b3dd666d0ef8facf6657a88a3f547c379987070f
Josh Rose <josh@etch.co>
2023-11-21 12:31:01 +0000

    feat: switch to our linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      extends: ["airbnb-typescript", "plugin:storybook/recommended"],
-      parserOptions: {
-        project: './tsconfig.json'
-      },
-      rules: {
-        "import/no-extraneous-dependencies": ["error", {
-          "devDependencies": true
-        }],
-        "react/jsx-props-no-spreading": "off"
-      }
-    }
-  ]
+  extends: ['@etchteam']
 };
