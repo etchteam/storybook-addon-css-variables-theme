c432ff72cc385c393bd3ba1ec628536aba56217f
Gavyn McKenzie <gavyn@etch.co>
2023-12-14 06:30:41 +0000

    fix: use dist preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["../preset.js", "@storybook/addon-essentials"],
+  "addons": ["../dist/preset.js", "@storybook/addon-essentials"],
   "framework": {
     name: "@storybook/react-webpack5",
     options: {}
